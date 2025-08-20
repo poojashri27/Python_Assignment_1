2d253d060501babb6e8e970a81d619ff86682142
Pooja Shrivastava <poojashrivastava216@gmail.com>
2025-08-20 23:23:05 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,21 @@
+# Task 1: Perform Basic Mathematical Operation
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+add = a + b
+sub = a - b
+multi = a * b
+div = a / b
+print("Addition: ",add)
+print("Subtraction: ", sub)
+print("Multiplication: ", multi)
+print("Division: ", div)
+
+
+# Task 2 : Create a personalized Greeting
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name + last_name
+
+print("Hello, "+ full_name+"!" + " Welcome to the Python program" )
